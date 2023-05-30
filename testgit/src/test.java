5b6c1d37a6135a5c1e1b61bd05b621b4e7b91312
Qiusengeng <81455214@qq.com>
2023-05-31 01:18:04 +0800

    第一次提交

--- testgit/src/test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        System.out.println("this is a test12");
-        System.out.println("success?");
-        System.out.println("nice~~~");
-        System.out.println("更新后再次上传");
-    }
-}
