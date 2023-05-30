ee222dd0598834ad43147921914e5be0e2631587
Qiusengeng <81455214@qq.com>
2023-05-31 00:40:24 +0800

    nice

--- testgit/src/test.java
+++ testgit/src/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("this is a test12");
         System.out.println("success?");
+        System.out.println("nice~~~");
     }
 }
